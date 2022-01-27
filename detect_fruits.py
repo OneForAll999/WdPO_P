576d19574aee3c96bb3f995d3ba08755ecaea240
OneForAll999 <kapsik99@gmail.com>
2022-01-27 13:15:11 +0100

    banan, orange

--- detect_fruits.py
+++ detect_fruits.py
@@ -52,6 +52,11 @@ def HSV_IMG(img):
 
 
 def detect_fruits(img_path: str) -> Dict[str, int]:
+    # TODO: Implement detection method.
+
+    apple = 0
+    banana = 0
+    orange = 0
     """Fruit detection function, to implement.
     Parameters
     ----------
@@ -62,65 +67,98 @@ def detect_fruits(img_path: str) -> Dict[str, int]:
     Dict[str, int]
         Dictionary with quantity of each fruit.
     """
-    # orange
-    img_orange = cv2.imread(img_path)
-    img_orange = cv2.cvtColor(img_orange, cv2.COLOR_BGR2HSV)
-    img_orange = cv2.resize(img_orange, (0, 0), fx=0.3, fy=0.3)
-    img_orange = cv2.GaussianBlur(img_orange, (11, 11), 0)
 
-    h, w, d = img_orange.shape
-    if h > w:
-        img_orange = cv2.rotate(img_orange, cv2.cv2.ROTATE_90_CLOCKWISE)
-    #HSV_IMG(img_orange)
-    hsv = cv2.cvtColor(img_orange, cv2.COLOR_BGR2HSV)
-    l_b = np.array([20, 237, 223])
-    u_b = np.array([255, 255, 255])
+    def detect_oranges():
+        # orange
+        img_orange = cv2.imread(img_path)
+        img_orange = cv2.cvtColor(img_orange, cv2.COLOR_BGR2HSV)
+        img_orange = cv2.resize(img_orange, (0, 0), fx=0.3, fy=0.3)
+        img_orange = cv2.GaussianBlur(img_orange, (181, 181), 0)
 
-    mask = cv2.inRange(hsv, l_b, u_b)
-    res = cv2.bitwise_and(img_orange, img_orange, mask=mask)
-    res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
-    to_show = res
-    res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+        h, w, d = img_orange.shape
+        if h > w:
+            img_orange = cv2.rotate(img_orange, cv2.cv2.ROTATE_90_CLOCKWISE)
 
-    ret, thresh = cv2.threshold(res, 90, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(to_show, contours, -1, (0, 255, 0), 3)
+        # HSV_IMG(img_orange)
+        hsv = cv2.cvtColor(img_orange, cv2.COLOR_BGR2HSV)
+        l_b = np.array([17, 231, 219])
+        u_b = np.array([255, 255, 255])
 
-    cv2.imshow('res', to_show)
-    cv2.waitKey(0)
-    #
+        mask = cv2.inRange(hsv, l_b, u_b)
+        kernel = np.ones((29, 29), np.uint8)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-    # circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20,
-    #                           param1=50, param2=30, minRadius=0, maxRadius=0)
-    # circles = np.uint16(np.around(circles))
-    # for i in circles[0, :]:
-    #     # draw the outer circle
-    #     cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    #     # draw the center of the circle
-    #     cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
-    # cv2.imshow('detected circles', cimg)
-    # cv2.waitKey(0)
-    # #Banan
-    # H_l = 19
-    # S_l = 100
-    # V_l =0
-    # H_A = 45
-    # S_A = 255
-    # V_A = 255
-    # banan_lower = np.array([H_l, S_l, V_l], np.uint8)
-    # banan_upper = np.array([H_A, S_A, V_A], np.uint8)
-    # banan_mask = cv2.inRange(img, banan_lower, banan_upper)
-    # output_orange = cv2.bitwise_and(img, img, mask=banan_mask)
-    # cv2.imshow('orange', output_orange)
-    # cv2.waitKey(0)
+        res = cv2.bitwise_and(img_orange, img_orange, mask=mask)
+        res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+        to_show = res
+        res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+
+        ret, thresh = cv2.threshold(res, 30, 255, 0)
+
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(to_show, contours, -1, (0, 255, 0), 5)
+        var = len(contours)
+        orange = var
+        return orange
+
+    def detect_bananas():
+        img_banana = cv2.imread(img_path)
+        img_banana = cv2.resize(img_banana, (0, 0), fx=0.3, fy=0.3)
+        img_banana = cv2.GaussianBlur(img_banana, (181, 181), 0)
+
+        h, w, d = img_banana.shape
+        if h > w:
+            img_banana = cv2.rotate(img_banana, cv2.cv2.ROTATE_90_CLOCKWISE)
+        # HSV_IMG(img_banana)#Banan
+        img_banana = cv2.cvtColor(img_banana, cv2.COLOR_BGR2HSV)
+        l_b = np.array([21, 104, 0])
+        u_b = np.array([26, 255, 255])
+        mask = cv2.inRange(img_banana, l_b, u_b)
+        kernel = np.ones((31, 31), np.uint8)
+        banana_mask = cv2.dilate(mask, kernel, iterations=1)
+        # banana_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        output_banana = cv2.bitwise_and(img_banana, img_banana, mask=banana_mask)
+        output_banana = cv2.cvtColor(output_banana, cv2.COLOR_HSV2BGR)
+        res = cv2.cvtColor(output_banana, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(res,123, 255, 0)
+        to_show = res
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        real_number_of_ctr = 0
+        for contour in contours:
+            if cv2.contourArea(contour) > 30000:
+                cv2.drawContours(output_banana, contour, -1, (255, 255, 255), 3)
+                real_number_of_ctr = real_number_of_ctr + 1
+        return real_number_of_ctr
+
+    def detect_apples():
+        img_apples = cv2.imread(img_path)
+        img_apples = cv2.resize(img_apples, (0, 0), fx=0.3, fy=0.3)
+        img_apples = cv2.GaussianBlur(img_apples, (11,11), 0)
+        # img_apples = cv2.blur(img_apples, (9, 9))
+
+        h, w, d = img_apples.shape
+        if h > w:
+            img_apples = cv2.rotate(img_apples, cv2.cv2.ROTATE_90_CLOCKWISE)
+        # HSV_IMG(img_apples)
+        img_apples = cv2.cvtColor(img_apples, cv2.COLOR_BGR2HSV)
+        l_b = np.array([0, 135, 0])
+        u_b = np.array([205, 255, 148])
+        mask = cv2.inRange(img_apples, l_b, u_b)
+        kernel = np.ones((51,51), np.uint8)
+        apple_mask = cv2.dilate(mask, kernel, iterations=1)
+        apple_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        output_apples = cv2.bitwise_and(img_apples, img_apples, mask=apple_mask)
+        output_apples = cv2.cvtColor(output_apples, cv2.COLOR_HSV2BGR)
+        cv2.imshow('a',output_apples)
+        cv2.waitKey(0)
+
+
     # ORange
-
-    # TODO: Implement detection method.
-
-    apple = 0
-    banana = 0
-    orange = 0
-
+    orange = detect_oranges()
+    banana = detect_bananas()
+    # detect_apples()
+    print(f' \n Oranges: {orange}, Bananas: {banana} \n')
     return {'apple': apple, 'banana': banana, 'orange': orange}
 
 
